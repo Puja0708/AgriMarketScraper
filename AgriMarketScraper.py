d7b56f62bce08da1be6186ea9fd971e5830dad53
Puja Singh <pujasingh@Pujas-MacBook-Air.local>
2025-07-25 22:41:52 +0530

    initial commit

--- AgriMarketScraper.py
+++ AgriMarketScraper.py
@@ -14,75 +14,94 @@ from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
 
-karnataka_major_crops = [
-    "Tomato",
-    "Onion",
-    "Potato",
-    "Maize",
-    "Paddy(Dhan)(Common)",
-    "Ragi (Finger Millet)",
-    "Green Gram (Moong)",
-    "Red Gram (Arhar/Tur)",
-    "Groundnut",
-    "Sunflower",
-    "Cotton",
-    "Chillies",
-    "Turmeric",
-    "Coriander",
-    "Beans",
-    "Brinjal",
-    "Cabbage",
-    "Carrot",
-    "Cauliflower",
-    "Coconut",
-    "Ginger",
-    "Garlic",
-    "Sweet Potato",
-    "Bajra (Pearl Millet)",
-    "Wheat",
-    "Sugarcane",
-    "Mango",
-    "Banana",
-    "Grapes",
-    "Sapota (Chikoo)",
-    "Papaya",
-    "Guava",
-    "Coffee"
-]
+# karnataka_major_crops = [
+#     "Tomato",
+#     "Onion",
+#     "Potato",
+#     "Maize",
+#     "Paddy(Dhan)(Common)",
+#     "Ragi (Finger Millet)",
+#     "Green Gram (Moong)",
+#     "Red Gram (Arhar/Tur)",
+#     "Groundnut",
+#     "Sunflower",
+#     "Cotton",
+#     "Chillies",
+#     "Turmeric",
+#     "Coriander",
+#     "Beans",
+#     "Brinjal",
+#     "Cabbage",
+#     "Carrot",
+#     "Cauliflower",
+#     "Coconut",
+#     "Ginger",
+#     "Garlic",
+#     "Sweet Potato",
+#     "Bajra (Pearl Millet)",
+#     "Wheat",
+#     "Sugarcane",
+#     "Mango",
+#     "Banana",
+#     "Grapes",
+#     "Sapota (Chikoo)",
+#     "Papaya",
+#     "Guava",
+#     "Coffee"
+# ]
+#
+# karnataka_districts = [
+#     "Bagalkot",
+#     "Ballari",
+#     "Belagavi",
+#     "Bengaluru Rural",
+#     "Bengaluru Urban",
+#     "Bidar",
+#     "Chamarajanagar",
+#     "Chikkaballapur",
+#     "Chikkamagaluru",
+#     "Chitradurga",
+#     "Dakshina Kannada",
+#     "Davanagere",
+#     "Dharwad",
+#     "Gadag",
+#     "Hassan",
+#     "Haveri",
+#     "Kalaburagi",
+#     "Kodagu",
+#     "Kolar",
+#     "Koppal",
+#     "Mandya",
+#     "Mysuru",
+#     "Raichur",
+#     "Ramanagara",
+#     "Shivamogga",
+#     "Tumakuru",
+#     "Udupi",
+#     "Uttara Kannada",
+#     "Vijayapura",
+#     "Yadgir",
+#     "Vijayanagara"
+# ]
 
 karnataka_districts = [
-    "Bagalkot",
-    "Ballari",
-    "Belagavi",
-    "Bengaluru Rural",
-    "Bengaluru Urban",
-    "Bidar",
-    "Chamarajanagar",
-    "Chikkaballapur",
-    "Chikkamagaluru",
-    "Chitradurga",
-    "Dakshina Kannada",
-    "Davanagere",
-    "Dharwad",
-    "Gadag",
-    "Hassan",
-    "Haveri",
-    "Kalaburagi",
-    "Kodagu",
-    "Kolar",
-    "Koppal",
-    "Mandya",
-    "Mysuru",
-    "Raichur",
-    "Ramanagara",
-    "Shivamogga",
-    "Tumakuru",
-    "Udupi",
-    "Uttara Kannada",
-    "Vijayapura",
-    "Yadgir",
-    "Vijayanagara"
+    "Bagalkot", "Ballari", "Belagavi", "Bengaluru", "Bengaluru Rural", "Bidar", "Chamarajanagar",
+    "Chikkaballapur", "Chikkamagaluru", "Chitradurga", "Dakshina Kannada", "Davanagere", "Dharwad",
+    "Gadag", "Hassan", "Haveri", "Kalaburagi", "Kodagu", "Kolar", "Koppal", "Mandya", "Mysuru",
+    "Raichur", "Ramanagara", "Shivamogga", "Tumakuru", "Udupi", "Uttara Kannada", "Vijayapura",
+    "Yadgir", "Vijayanagara"
 ]
+
+karnataka_crops = [
+     "Avare Dal", "Avare Whole", "Banana", "Beans", "Beetroot", "Bitter gourd",
+    "Bottlegourd", "Brinjal", "Cabbage", "Capsicum", "Carrot", "Cauliflower", "Chayote", "Chillies Dry",
+    "Chow Chow", "Cluster beans", "Coconut", "Coriander (Dhania)", "Cucumber", "Field Pea (Dry)",
+    "Field Pea (Green)", "French Beans (Frasbean)", "Garlic", "Ginger(Dry)", "Green Chilli", "Green Gram (Moong)",
+    "Guar", "Guava", "Knool Khol", "Ladies Finger", "Lemon", "Little gourd (Kundru)", "Maize", "Mango", "Onion",
+    "Papaya", "Peas cod", "Potato", "Pumpkin", "Radish", "Ridgeguard(Tori)", "Snakeguard", "Sweet Potato",
+    "Tamarind Fruit", "Tomato", "Water Melon", "Yam"
+]
+
 
 
 def fetch_data(state="Karnataka"):
@@ -92,30 +111,16 @@ def fetch_data(state="Karnataka"):
     # Select from dropdowns: state, commodity, date range etc
     # Loop through commodities of interest
     results = []
-    for crop in karnataka_major_crops:  # e.g. ["Tomato","Onion","Ragi",...]
+    for crop in karnataka_crops:  # e.g. ["Tomato","Onion","Ragi",...]
 
-
-        # wait = WebDriverWait(driver, 15)
         state_dropdown = Select(driver.find_element(By.ID, "ddlState"))
-        # state_dropdown.select_by_visible_text("Karnataka")
-
-        # state_dropdown = Select(driver.find_element(By.ID, "cphBody_State"))
-
-        # state_dropdown = Select(wait.until(EC.presence_of_element_located((By.ID, "cphBody_State"))))
-
-        # state_dropdown = Select(driver.find_element(By.ID, "cphBody_State"))
         state_dropdown.select_by_visible_text(state)
-
-        market_dropdown = Select(driver.find_element(By.ID, "ddlMarket"))
-        # market_dropdown.select_by_visible_text("Your Market Name Here")
-        #
-        # commodity_dropdown = Select(driver.find_element(By.ID, "cphBody_Commodity"))
-        market_dropdown.select_by_visible_text(crop)
-
-        # select_state(state)
-        # select_commodity(crop)
-        # apply_filters()
+        # market_dropdown = Select(driver.find_element(By.ID, "ddlMarket"))
+        crop_dropdown_element = driver.find_element(By.ID, "ddlCommodity")
+        crop_dropdown = Select(crop_dropdown_element)
+        crop_dropdown.select_by_visible_text(crop)
         soup = BeautifulSoup(driver.page_source, "html.parser")
+
         table = soup.find("table", {"id":"ContentPlaceHolder1_gvData"})
         for row in table.find_all("tr")[1:]:
             cols = row.find_all("td")
@@ -125,7 +130,7 @@ def fetch_data(state="Karnataka"):
                 "market_selling_price": float(cols[7].text.strip())
             })
     driver.quit()
-    print(json.dumps(results, indent=2))
+    # print(json.dumps(results, indent=2))
     with open("karnataka_crop_market_prices.json", "w") as f:
         json.dumps(results, indent=2)
 
